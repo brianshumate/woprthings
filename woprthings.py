3bd0478910aaad2c75ccabdf7c812e9418be4dd0
Brian Shumate <brian@brianshumate.com>
2015-07-12 15:45:43 -0400

    last commit of all time

---
+++ woprthings.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import os
+import random
+import time
+from twython import Twython
+from twython import TwythonError
+
+myhandle = 'woprthings'
+
+def auth():
+    with open("etc/access.json", 'r') as access:
+        db = json.load(access)
+    return Twython(db["API_Key"], db["API_Secret"], db["Access_Token"], db["Access_Token_Secret"])
+
+def get_phrase():
+    with open('share/dialogue.json', 'r') as dialogue_json:
+        dialogue_list = json.load(dialogue_json)
+        dialogue = dialogue_list['dialogphrases']
+        phrase = dialogue[random.randint(0, len(dialogue) - 1)]
+        return phrase
+
+def main():
+    print '[i] @{myhandle} ACTIVATE!'.format(myhandle=myhandle)
+    twutt_text = get_phrase()
+    print '[!] Gonna twutt: {twutt_text}'.format(twutt_text=twutt_text)
+    twitter = auth()
+    twitter.update_status(status=twutt_text)
+
+if __name__ == "__main__":
+    main()
